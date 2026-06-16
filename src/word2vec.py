aea28ecec8ca7376e639565d6ad37ea88663db69
Kavindi Kariyawasam <kavindidilshara7@gmail.com>
2026-06-16 23:00:44 +0530

    Added word2vec file

--- src/word2vec.py
+++ src/word2vec.py
@@ -1,1 +1,6 @@
 from gensim.models import Word2Vec
+
+def train_word2vec(texts):
+    sentences = [text.split() for text in texts]
+    model = Word2Vec(sentences, vector_size=100, window=5, min_count=1)
+    return model
